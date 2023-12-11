185945a77f2742b0ad5f6c4dc6dd710b6e1fa2be
qwer7824 <sh7824@naver.com>
2023-12-11 18:18:09 +0900

    Build error

--- src/main/java/com/pos/controller/ProductController.java
+++ src/main/java/com/pos/controller/ProductController.java
@@ -14,8 +14,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.validation.Valid;
-import java.util.List;
 @Controller
 @Slf4j
 @RequiredArgsConstructor
